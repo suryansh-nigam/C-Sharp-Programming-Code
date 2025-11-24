45ec3ec6abe403b218b9f261df7badff30ed1fe3
Suryansh Nigam <131799265+suryansh-nigam@users.noreply.github.com>
2025-11-24 11:12:52 +0530

    Create OopsPillars.cs

---
+++ OopsPillars.cs
@@ -0,0 +1,109 @@
+1) Encapsulation Example
+---------------------------------------------------------------------
+
+using System;
+
+class Student{
+    
+    private int age;
+    
+    public void SetAge(int a){
+        if(a>0){
+            age = a;
+        }
+    }
+    
+    public int GetAge(){
+        return age;
+    }
+    
+    static void Main(){
+        Student s = new Student();
+        s.SetAge(22);
+        Console.Write(s.GetAge());
+    }
+}
+
+============================================================================
+
+2) Inheritance Example
+---------------------------------------------------------------------
+
+using System;
+
+class Animal{
+    
+    public void Sound(){
+       Console.Write("Animal Sound");
+        }
+   
+   class Dog : Animal {
+       
+   }
+    static void Main(){
+        Dog d = new Dog();
+        d.Sound();
+    
+    }
+}
+
+==============================================================================
+
+3) Polymorphism Example
+----------------------------------------------------------------------
+  
+  --- > Method Overloading
+
+using System;
+
+class Math{
+    public int Add(int a,int b){
+        return a +b;
+    }
+    
+    public int Add(int a,int b, int c){
+        return a +b + c;
+    }
+    
+     public int Add(int a,int b, int c, int d){
+        return a +b + c+ d;
+    }
+    
+    static void Main(){
+     Math obj = new Math();
+        Console.WriteLine(obj.Add(2,3)); // 5
+        Console.WriteLine(obj.Add(2,3,4)); // 9
+        Console.WriteLine(obj.Add(2,3,4,5)); // 14
+    }
+}
+
+
+  --- > Method Overriding
+
+  using System;
+
+class Animal
+{
+    public virtual void Speak()
+    {
+        Console.WriteLine("Animal makes a sound");
+    }
+}
+
+class Dog : Animal
+{
+    public override void Speak()
+    {
+        Console.WriteLine("Dog barks");
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        Animal obj = new Dog();   // upcasting
+        obj.Speak();              // Output: Dog barks
+    }
+}
+
